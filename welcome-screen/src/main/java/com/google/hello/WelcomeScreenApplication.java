c87ca1c1b1d71102cd9754175902b3dccd796e4b
Deenanathsingh <deenanath.singh.549@gmail.com>
2019-04-15 15:00:54 +0530

    First Commit

---
+++ welcome-screen/src/main/java/com/google/hello/WelcomeScreenApplication.java
@@ -0,0 +1,13 @@
+package com.google.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomeScreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomeScreenApplication.class, args);
+	}
+
+}
